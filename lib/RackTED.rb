f6e651cd699c060edf6f3e5522072782831287cf
valentinerutto <rutto28@gmail.com>
2015-08-21 11:26:33 +0300

    added form to index file

--- lib/RackTED.rb
+++ lib/RackTED.rb
@@ -1,13 +1,16 @@
 require "erb"
-class RackTED
+class RackTED 
   def call(env)
       request=Rack::Response.new(env)
       case request.path
-      when"/" then Rack::Response.new(render("index.html.erb"))
+      when "/" then 
+      Rack::Response.new(render("index.html.erb"))
       else Rack::Response.new(render("Not Found",404))
+      end
   end
+          
   def render(template)
-    path=File.expand_path("../views/#{template}",__FILE__)
-      ERB.new(File.read(path)).result(binding)
+   path=File.expand_path("../views/#{template}",__FILE__)
+      ERB.new(File.read(loc)).result(binding)
   end
 end
