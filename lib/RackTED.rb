343a059bac5ecfc4c0cd0e7a832d5ae9810c7ddf
valentinerutto <rutto28@gmail.com>
2015-08-21 15:54:12 +0300

    show user request on path change

--- lib/RackTED.rb
+++ lib/RackTED.rb
@@ -3,14 +3,15 @@ class RackTED
   def call(env)
       request=Rack::Response.new(env)
       case request.path
-      when "/" then 
-      Rack::Response.new(render("index.html.erb"))
-      else Rack::Response.new(render("Not Found",404))
-      end
+      when "/" then Rack::Response.new(render("index.html.erb"))
+      when "/change" then Rack::Response.new(request.params["name"])
+      else
+          Rack::Response.new(render("Not Found",404))
+  end
   end
           
   def render(template)
-   path=File.expand_path("../views/#{template}",__FILE__)
-      ERB.new(File.read(loc)).result(binding)
+   path = File.expand_path("../views/#{template}",__FILE__)
+      ERB.new(File.read(path)).result(binding)
   end
 end
