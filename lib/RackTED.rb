f4eb0ab3da90960ed1036ea1a057d21a489a383d
valentinerutto <rutto28@gmail.com>
2015-08-21 08:16:49 +0300

    called result with binding parameter on erb so as to get acces to defined methods

--- lib/RackTED.rb
+++ lib/RackTED.rb
@@ -5,6 +5,6 @@ Rack::Response.new (render("index.html.erb"))
   end
   def render(template)
     path=File.expand("../views/#{template}",_FILE_)
-      ERB.new(File.read(path))
+      ERB.new(File.read(path)).result(binding)
   end
 end
